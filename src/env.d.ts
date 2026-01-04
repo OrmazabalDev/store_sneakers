96cc2196b28419437865ef510b49150a702b283a
Panxito24 <diego.ormazabal@correoaiep.cl>
2026-01-04 16:56:09 -0300

    fix 2

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ declare interface ImportMetaEnv {
   readonly VITE_WHATSAPP_NUMBER: string;
   readonly VITE_SHEET_ID: string;
   readonly VITE_GOOGLE_API_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY: string;
 }
 
 declare interface ImportMeta {
